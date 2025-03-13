abf225d7998be3a01c5313328df5641abbf6e39a
Gppsv143 <praveenkumar04479@gmail.com>
2025-03-13 05:51:53 +0000

    Deleted old backend and frontend folders

--- backend/models/User.js
+++
@@ -1,5 +0,0 @@
-const User = {
-  createUser: 'INSERT INTO users (name, email, phone, password) VALUES (?, ?, ?, ?)',
-  findUserByPhone: 'SELECT * FROM users WHERE phone = ?'
-};
-module.exports = User;
