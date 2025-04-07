3bab3be28f4721c30130d7539a63fb870336635f
root <root@ip-10-1-1-46.ec2.internal>
2025-04-07 08:40:03 +0000

    Added PraveenBus logo and updated homepage

--- frontend/src/components/Footer.js
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="bg-gray-800 text-white text-center p-4 mt-4">
-      <p>© 2025 PraveenBus. All Rights Reserved.</p>
-    </footer>
-  );
-}
